5d924a5f24d71dfc0aa2d31dc6c0c423779a760b
akameco <akameco.t@gmail.com>
2018-06-17 23:44:56 +0900

    docs(readme): update

--- cli.js
+++ cli.js
@@ -10,8 +10,10 @@ const cli = meow(`
 
 	Examples
 	  $ sana-voice
+
 	  $ sana-voice --title
 	  看護看護の日…とは言ったものの/アクリルフィギュア03
+
 	  $ sana-voice --link
 	  http://sanabutton.ojaru.jp/.%2Fsound%2F%E7%9C%8B%E8%AD%B7%E3%81%AE%E6%97%A5%E2%80%A6%E3%81%A8%E3%81%AF%E8%A8%80%E3%81%A3%E3%81%9F%E3%82%82%E3%81%AE%E3%81%AE%2F%E3%82%A2%E3%82%AF%E3%83%AA%E3%83%AB%E3%83%95%E3%82%A3%E3%82%AE%E3%83%A5%E3%82%A203.mp3
 `)
