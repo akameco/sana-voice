390fef321a170afff292c163d43635d55da7a1bb
akameco <akameco.t@gmail.com>
2018-06-19 19:18:22 +0900

    refactor(cache): use symphogear cache

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 // @flow
-const fs = require('fs')
-const got = require('got')
 const symphogear = require('symphogear')
 const tempWrite = require('temp-write')
 const VOICE_LIST /*: Array<{ title: string, link: string }> */ = require('./sana-voice-list.json')
-
-const TMP_FILE = 'sana.mp3'
 
 const findVoice = (query /*: string */) =>
   VOICE_LIST.filter(voice => voice.title.includes(query))
@@ -16,10 +12,7 @@ const sanaVoice = async (query /*: ?string */) => {
     throw new Error(`${query} does not match`)
   }
   const item = list[Math.floor(Math.random() * list.length)]
-  const response = await got(item.link, { encoding: null })
-  const filePath = tempWrite.sync(TMP_FILE)
-  fs.writeFileSync(filePath, response.body)
-  await symphogear(filePath)
+  await symphogear(item.link)
   return item
 }
 
